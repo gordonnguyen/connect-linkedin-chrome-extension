f92abc0be53b74bbd3119ab7957245f72a10237b
Gordon Nguyen <gordon-nguyen@outlook.com>
2024-11-26 21:25:32 -0600

    Rewrite logic using proper polling technique

--- content.js
+++ content.js
@@ -1,20 +1,56 @@
-let buttonExist = setInterval(() => {
-  const buttons = document.querySelectorAll('button');
-  for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-  
-    if (button.innerText.includes('Connect')) {
-      button.click();
-      console.log(button);
-      console.log('Clicked Connect button');
-      clearInterval(buttonExist);
-      break;  // Exit the loop once the button is found
+// Set polling rate to check if the element exist
+const pollingInterval = 5000; // 5 seconds in milliseconds
+const maxPollingDuration = 30000; // 30 seconds in milliseconds
+
+const startTime = Date.now(); // Record the start time
+
+const clickButton = async (text) => {
+  try {
+    console.log('Checking for button with text:', text);
+    const buttons = document.querySelectorAll('button');
+
+    const isButtonFound = () => {
+      console.log('Loop start');
+      for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        
+        if (button.innerText.includes(text)) {
+          if(text == 'Connect') {button.click();}
+          console.log(button);
+          console.log('Clicked button', text);
+          clearInterval(clickButton);
+          return true;  // Exit the loop once the button is found
+        }
+      }
+    };
+
+    if (isButtonFound()) {
+      return true;
+    }
+    console.log('Check not found');
+
+
+    // Check polling
+    const elapsedTime = Date.now() - startTime;
+      if (elapsedTime < maxPollingDuration) {
+          setTimeout(() => clickButton(text), pollingInterval); // Schedule next request
+      } else {
+          console.log('Maximum polling duration reached. Stopping polling.');
+      }
+
+  }
+  catch (error) {
+    console.error('Button not found yet. Retrying...', error);
+    const elapsedTime = Date.now() - startTime;
+
+    if (elapsedTime < maxPollingDuration) {
+        setTimeout(() => clickButton(text), pollingInterval); // Schedule next request
+    } else {
+        console.log('Maximum polling duration reached. Stopping polling.');
     }
   }
-}, 100);
+}
 
-// Time out after 30 seconds
-setTimeout(() => {
-  clearInterval(buttonExist);
-  console.log('Program stopped')
-}, 30000);
+if (clickButton('Connect')) {
+  clickButton('Send');
+};
